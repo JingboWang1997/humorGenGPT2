79d8945b2d11403283e42a9c3ac13d58a623b69a
Twister <kcaras6@gatech.edu>
2019-09-19 14:03:51 -0400

    movies and supreme

--- gpt2_simple/finetune.py
+++ gpt2_simple/finetune.py
@@ -1,13 +1,13 @@
 import gpt_2_simple as gpt2
 
-model_name = "117M"
+model_name = "355M"
 gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/117M/
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
-              'brown_humor.txt',
+              '../../Data/movies.txt',
               model_name=model_name,
-	      run_name='humor',
-              steps=1000)   # steps is max number of training steps
+	      run_name='movies',
+              steps=10000) # steps is max number of training steps
 
 gpt2.generate(sess)
--- gpt2_simple/generate.py
+++ gpt2_simple/generate.py
@@ -4,4 +4,8 @@ print('generating');
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
 
-gpt2.generate(sess)
+gpt2.generate(sess, prefix="Could you please tell me how you're defining 'scandalous mark'? From your brief, I thought you were giving it a different definition than")
+
+
+
+
