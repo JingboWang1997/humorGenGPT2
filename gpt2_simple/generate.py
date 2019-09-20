28b9d889c953f23519e1cf96ad7c844837d63a45
JingboWang1997 <wjbjimmy@gmail.com>
2019-09-20 01:42:33 -0400

    generate

--- gpt2_simple/generate.py
+++ gpt2_simple/generate.py
@@ -2,9 +2,13 @@ import gpt_2_simple as gpt2
 
 sess = gpt2.start_tf_sess()
 print('Start Generating')
-gpt2.load_gpt2(sess, run_name='scifi')
-for i in range(3):
-#    gpt2.generate(sess, length=20);
-#    print('\n')
-    gpt2.generate(sess, length=100, prefix="At least he didn't yell at her -- I told her he didn't have a heart of stone");
+prefixes = ['Nurse Chang discusses the progress of her research on the planet, and the thought of a female Human',
+            'However, when she overrules him, the Doctor realizes that the Doctor has been wrong, stating that',
+            "A team of undercover security personnel from the FBI infiltrate a warehouse in a warehouse in Los Angeles' Central"]
+gpt2.load_gpt2(sess, run_name='run1')
+for prefix in prefixes:
+    print('\nprefix:\n',prefix)
+    for i in range(3):
+        gpt2.generate(sess, length=100, prefix=prefix);
+        print('\n')
 		
