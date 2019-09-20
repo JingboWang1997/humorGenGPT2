d59f32f6f9bfb92d68b40c197131e55db1120fd9
Twister <kcaras6@gatech.edu>
2019-09-20 01:24:56 -0400

    scifi

--- gpt2_simple/generate.py
+++ gpt2_simple/generate.py
@@ -1,11 +1,10 @@
 import gpt_2_simple as gpt2
 
-print('generating');
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess)
-
-gpt2.generate(sess, prefix="Could you please tell me how you're defining 'scandalous mark'? From your brief, I thought you were giving it a different definition than")
-
-
-
-
+print('Start Generating')
+gpt2.load_gpt2(sess, run_name='scifi')
+for i in range(3):
+#    gpt2.generate(sess, length=20);
+#    print('\n')
+    gpt2.generate(sess, length=100, prefix="At least he didn't yell at her -- I told her he didn't have a heart of stone");
+		
